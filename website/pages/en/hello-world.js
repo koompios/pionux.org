e2814b8c61eb3dad4a3f7a642678946053f67111
Densovan <sarimramata@gmail.com>
2019-11-24 09:51:36 +0700

    third update

--- website/pages/en/hello-world.js
+++ website/pages/en/hello-world.js
@@ -1,17 +1,30 @@
-const React = require('react');
+import React, { useState } from 'react'
+
 
 const CompLibrary = require('../../core/CompLibrary.js');
 
 const Container = CompLibrary.Container;
 const GridBlock = CompLibrary.GridBlock;
 
-function HelloWorld(props) {
+function HelloWorld() {
+  const [count, setCount] =useState(1);
+    const counHandle = () => {
+      setCount(count+1);
+      console.log(setCount);
+    }
+   
   return (
-    <div className="docMainWrapper wrapper">
-      <Container className="mainContainer documentContainer postContainer">
-        <h1>Hello World!</h1>
-        <p>This is my first pag</p>
-      </Container>
+    // <div className="docMainWrapper wrapper">
+    //   <Container className="mainContainer documentContainer postContainer">
+    //     <h1>Hello World!</h1>
+    //     <p>This is my first pag</p>
+    //     <h1>{count}</h1>
+    //     <button onClick = {counHandle}>Click me</button>
+    //   </Container>
+    // </div>
+    <div>
+      <h1>{count}</h1>
+      <button onClick={counHandle}>Click</button>
     </div>
   );
 }
